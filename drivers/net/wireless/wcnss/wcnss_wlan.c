9ad7e1c2c8175893c669f1b71eb2295e608c9f17
Dan Pasanen <dan.pasanen@gmail.com>
2015-07-06 18:30:55 -0500

    prima: update from VK810 35A release
    * also update wcnss nv filename
    
    Change-Id: I2a3a267d2a30e30c12bdc1f891abf028c5e7b9b5

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -136,7 +136,7 @@ static struct wcnss_pmic_dump wcnss_pmic_reg_dump[] = {
 	{"LVS1", 0x060}, /*LVS7*/
 };
 
-#define NVBIN_FILE "wlan/prima/WCNSS_qcom_wlan_nv.bin"
+#define NVBIN_FILE "wlan/prima/WCNSS_qcom_wlan_nv_init.bin"
 
 /*
  * On SMD channel 4K of maximum data can be transferred, including message
